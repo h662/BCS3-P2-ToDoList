50246b50aa248f09a8ea7d2fa4291d934beec0d9
h662Olbm <h662@olbm.app>
2023-04-13 06:57:00 +0900

    user router

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,15 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
+const userRouter = require("./routes/user");
+
 const app = express();
 
 const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
+app.use("/user", userRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello, Express!");
