02e082635ba8f1c9ed3c23d6191480a755b9f47d
h662Olbm <h662@olbm.app>
2023-04-13 20:47:58 +0900

    Update done

--- frontend/src/components/TodoCard.jsx
+++ frontend/src/components/TodoCard.jsx
@@ -12,19 +12,32 @@ const TodoCard = ({ todo, isDone, id, getTodos }) => {
     }
   };
 
+  const onClickDone = async () => {
+    try {
+      await axios.put(`${process.env.REACT_APP_BACKEND_URL}/todo/${id}/done`);
+
+      getTodos();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="flex my-4">
       {isDone ? (
         <>
-          <div className="relative">
+          <button className="relative" onClick={onClickDone}>
             <div className="border-4 border-pink-400 w-8 h-8 rounded-xl bg-pink-400 p-2"></div>
             <div className="absolute border-4 border-white top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 w-8 h-8 scale-75 rounded-xl bg-pink-400 p-2"></div>
-          </div>
+          </button>
           <div className="text-2xl ml-4 truncate">{todo}</div>
         </>
       ) : (
         <>
-          <div className="border-4 border-pink-400 w-8 h-8 rounded-xl"></div>
+          <button
+            className="border-4 border-pink-400 w-8 h-8 rounded-xl"
+            onClick={onClickDone}
+          ></button>
           <div className="text-2xl ml-4 truncate">{todo}</div>
         </>
       )}
